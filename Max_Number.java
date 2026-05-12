8a432f1e8e6eef810c5569b4dc924121355b2f9e
Sajjadul Islam <sajjadul625@gmail.com>
2026-05-12 11:31:17 +0600

    Max Number

---
+++ Max_Number.java
@@ -0,0 +1,13 @@
+class Main{
+    pubic static void main(String[] args){
+        int arr [] = {1, 2, 3, 4, 5};
+        int max = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println("The maximum number in the array is: " + max);
+    }
+}
