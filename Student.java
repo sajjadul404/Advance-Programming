6ee345b1e207ae27f114c159e3607a3cabb491b9
Sajjadul Islam <sajjadul625@gmail.com>
2026-05-22 22:04:03 +0600

    Student

---
+++ Student.java
@@ -0,0 +1,29 @@
+class Student
+{
+    String name;
+    int roll;
+    Student(String name,int roll)
+    {
+        this.name = name;
+        this.roll = roll;
+    }
+    Student(int roll,String name)
+    {
+        this.name = name;
+        this.roll = roll;
+    }
+    void display()
+    {
+        System.out.println("Student name: "+name+" "+"Roll: "+roll);
+    }
+}
+class Main
+{
+    public static void main(String args[])
+    {
+        Student s1=new Student("ABC",3);
+        Student s2 = new Student(12,"VBD");
+        s1.display();
+        s2.display();
+    }
+}
