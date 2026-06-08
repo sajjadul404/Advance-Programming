af664ae837cf66d97a9cc2a363c53aae9901e5bd
Sajjadul Islam <sajjadul625@gmail.com>
2026-06-08 23:37:19 +0600

    Stack Update

--- Stack/Main.java
+++ Stack/Main.java
@@ -5,14 +5,13 @@ class Stack {
     private int top;
     private int size;
 
-    // Constructor
+  
     Stack(int size) {
         this.size = size;
         stack = new int[size];
         top = -1;
     }
 
-    // Push method
     void push(int value) {
         if (top == size - 1) {
             System.out.println("Stack Overflow! Cannot push " + value);
@@ -23,7 +22,7 @@ class Stack {
         }
     }
 
-    // Pop method
+
     int pop() {
         if (top == -1) {
             System.out.println("Stack Underflow! Stack is empty.");
@@ -35,7 +34,7 @@ class Stack {
         }
     }
 
-    // Display method
+
     void display() {
         if (top == -1) {
             System.out.println("Stack is empty.");
@@ -51,23 +50,20 @@ class Stack {
 public class Main {
     public static void main(String[] args) {
 
-        // Create Stack object
+     
         Stack s = new Stack(5);
 
-        // Push elements
+   
         s.push(10);
         s.push(20);
         s.push(30);
         s.push(40);
 
-        // Display stack
         s.display();
 
-        // Pop elements
         System.out.println("\nPopped Element: " + s.pop());
         System.out.println("Popped Element: " + s.pop());
 
-        // Display stack after pop
         System.out.println();
         s.display();
     }
