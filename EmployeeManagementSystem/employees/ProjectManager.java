2a60240567e7384e4fb24a7fe6afbcb3a2f9d854
Sajjadul Islam <sajjadul625@gmail.com>
2026-07-22 02:09:48 +0600

    Project Manager

---
+++ EmployeeManagementSystem/employees/ProjectManager.java
@@ -0,0 +1,30 @@
+package managers;
+
+import employees.FullTimeEmployee;
+
+public class ProjectManager extends FullTimeEmployee implements Manager {
+    private String department;
+
+    public ProjectManager(String name, String position, double salary, String department) {
+        super(name, position, salary);
+        this.department = department;
+    }
+
+    public void assignTask(String task) {
+        System.out.println("Project Manager " + name + " from " + department + 
+                           " department assigned task: " + task);
+    }
+
+    public void performTask() {
+        System.out.println(name + " is managing the " + department + " department and assigning tasks.");
+    }
+
+    public String getEmployeeDetails() {
+        return super.getEmployeeDetails() + "\n" +
+               "Department: " + department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+}
