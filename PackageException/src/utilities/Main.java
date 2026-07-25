da89181bfd8a90d92eb0bbb79b18241d17789066
Sajjadul Islam <sajjadul625@gmail.com>
2026-07-26 02:55:02 +0600

    main function

--- PackageException/src/utilities/Main.java
+++ PackageException/src/utilities/Main.java
@@ -6,7 +6,6 @@ import utilities.ShapeUtility;
 
 public class Main {
     public static void main(String[] args) {
-        
         Printable[] shapes = new Printable[] {
             new Rectangle(5.0, 4.0),  
             new Rectangle(3.0, 2.0),
