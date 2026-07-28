a4280aec14670779414e006da0c2fedc51faf9ad
Sajjadul Islam <sajjadul625@gmail.com>
2026-07-29 00:42:44 +0600

    Banned

---
+++ Banned.java
@@ -0,0 +1,15 @@
+package String;
+
+public class Banned {
+    
+       public static void main (String[]args){
+
+      
+        String comment = "This is a spam message";
+
+        System.out.println(comment.contains("spam") || comment.contains("ads") );
+        
+
+    }
+
+}
