be04f31aee3e734fc9bbc85006b550ef9d5c3430
Sajjadul Islam <sajjadul625@gmail.com>
2026-07-31 02:42:36 +0600

    ChlorineOutOfRangeException comment remove

--- Water Management System/ChlorineOutOfRangeException.java
+++ Water Management System/ChlorineOutOfRangeException.java
@@ -1,7 +1,3 @@
-/**
- * Thrown when the chlorine concentration falls outside the
- * safe disinfection range of 0.2 - 1.0 mg/L.
- */
 public class ChlorineOutOfRangeException extends Exception {
     public ChlorineOutOfRangeException(String message) {
         super(message);
