da84feb822c4ea5c312731469d3570399bbc4acc
Sajjadul Islam <sajjadul625@gmail.com>
2026-08-01 05:36:48 +0600

    HighTurbidityException

--- Water Management System/HighTurbidityException.java
+++ Water Management System/HighTurbidityException.java
@@ -1,7 +1,4 @@
-/**
- * Thrown when turbidity (water cloudiness) exceeds the safe
- * limit of 5 NTU, indicating poor water quality.
- */
+
 public class HighTurbidityException extends Exception {
     public HighTurbidityException(String message) {
         super(message);
