a591e4eea7f92e0bba41efa926502b40be2aaab5
Sajjadul Islam <sajjadul625@gmail.com>
2026-08-06 23:32:15 +0600

    PumpOfflineException

--- Water Management System/PumpOfflineException.java
+++ Water Management System/PumpOfflineException.java
@@ -1,7 +1,3 @@
-/**
- * Thrown when a monitoring zone's pump status is "OFFLINE",
- * meaning water is not currently being distributed/monitored live.
- */
 public class PumpOfflineException extends Exception {
     public PumpOfflineException(String message) {
         super(message);
